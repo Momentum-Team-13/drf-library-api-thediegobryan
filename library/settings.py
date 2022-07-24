9b8e241e1c7099ef56ca66364bbdf738496bbb4b
Diego Bryan <thediegobryan@gmail.com>
2022-07-24 13:08:37 -0400

    imported heroku

--- library/settings.py
+++ library/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import environ
-import os
+import django_on_heroku
 
 env = environ.Env(
     # set casting, default value
@@ -129,8 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Heroku
+django_on_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
